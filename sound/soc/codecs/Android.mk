29988abb3faa13142044d18f5691fc988f137415
vkarp <vkarp@motorola.com>
2019-08-29 18:49:39 -0500

    ASoC: cirrus-playback: Add cirrus playback driver
    This is kernel 4.14 driver updated to 4.19 API
    
    Change-Id: I5dcd3e69786017fd56575fb0d5af32eae6ed6062
    Signed-off-by: vkarp <vkarp@motorola.com>
    Signed-off-by: Ravi Vembu <Raviv@motorola.com>
    
    Mot-CRs-Fixed: (CR)
    Change-Id: I5dcd3e69786017fd56575fb0d5af32eae6ed6062
    Reviewed-on: https://gerrit.mot.com/1445635
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Ravikumar Vembu <Raviv@motorola.com>
    Submit-Approved: Jira Key

--- sound/soc/codecs/Android.mk
+++ sound/soc/codecs/Android.mk
@@ -26,13 +26,13 @@ LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := cirrus_cs47l35.ko
+LOCAL_MODULE := cirrus_cs35l36.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := cirrus_cs35l36.ko
+LOCAL_MODULE := cirrus_cs47l35.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
@@ -56,6 +56,12 @@ else ifeq ($(CIRRUS_AMP_CODEC),cs35l41_i2c)
 KERNEL_CFLAGS += CONFIG_SND_SOC_CS35L41_I2C=y
 endif
 LOCAL_MODULE := cirrus_cs35l41.ko
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/AndroidKernelModule.mk
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := cirrus_playback.ko
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/AndroidKernelModule.mk
