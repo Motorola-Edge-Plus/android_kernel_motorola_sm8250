38344476870bee59f123d8a3cfb979e23073f98c
Cosmin Tanislav <demonsingur@gmail.com>
2022-06-26 23:23:58 +0300

    techpack: display: dsi_panel: remove panel notifications
    Change-Id: Id01440a28bdfe0b8e51be8b1f9ffef69b4d38148

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -12,13 +12,6 @@
 
 #if defined(CONFIG_PANEL_NOTIFICATIONS)
 #include <linux/panel_notifier.h>
-
-#define PANEL_NOTIFY(note) { \
-	int idx = dsi_panel_get_index(panel); \
-	panel_notify(note, (void *)&idx); \
-}
-#else
-#define PANEL_NOTIFY(...)
 #endif
 
 #include "dsi_display.h"
@@ -5173,19 +5166,6 @@ exit:
 	return rc;
 }
 
-#if defined(CONFIG_PANEL_NOTIFICATIONS)
-static inline int dsi_panel_get_index(struct dsi_panel *panel)
-{
-	struct dsi_display *dsi_display =
-			container_of(panel->host, struct dsi_display, host);
-
-	if (unlikely(dsi_display == NULL))
-		return -EINVAL;
-
-	return dsi_display->display_idx;
-}
-#endif
-
 int dsi_panel_prepare(struct dsi_panel *panel)
 {
 	int rc = 0;
@@ -5219,7 +5199,6 @@ int dsi_panel_prepare(struct dsi_panel *panel)
 		       panel->name, rc);
 		goto error_disable_gpio;
 	}
-	PANEL_NOTIFY(PANEL_EVENT_PANEL_PREPARE);
 
 	goto end;
 error_disable_gpio:
@@ -5733,8 +5712,6 @@ int dsi_panel_enable(struct dsi_panel *panel)
 
 	panel->panel_initialized = true;
 
-	PANEL_NOTIFY(PANEL_EVENT_PRE_DISPLAY_ON);
-
 err:
 
 	mutex_unlock(&panel->panel_lock);
@@ -5767,8 +5744,6 @@ int dsi_panel_post_enable(struct dsi_panel *panel)
 		}
 	}
 
-	PANEL_NOTIFY(PANEL_EVENT_DISPLAY_ON);
-
 error:
 	mutex_unlock(&panel->panel_lock);
 	return rc;
@@ -5791,8 +5766,6 @@ int dsi_panel_pre_disable(struct dsi_panel *panel)
 		       panel->name, rc);
 		goto error;
 	}
-
-	PANEL_NOTIFY(PANEL_EVENT_PRE_DISPLAY_OFF);
 
 error:
 	mutex_unlock(&panel->panel_lock);
@@ -5837,8 +5810,6 @@ int dsi_panel_disable(struct dsi_panel *panel)
 	}
 	panel->panel_initialized = false;
 	panel->power_mode = SDE_MODE_DPMS_OFF;
-
-	PANEL_NOTIFY(PANEL_EVENT_DISPLAY_OFF);
 
 	mutex_unlock(&panel->panel_lock);
 	return rc;
